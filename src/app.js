dfc1d6d831c1744dbed1f62d2a9c5a4584075363
Kristina <kzatesova@gmail.com>
2022-05-17 16:23:14 +0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import './styles.css'
+import { isValid } from './utils'
+
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const submitBtn = form.querySelector('#submit')
+
+form.addEventListener('submit', submitFormHandler)
+input.addEventListener()
+
+function submitFormHandler(event) {
+    event.preventDefault()
+
+    if (isValid(input.value)) {
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+
+        submitBtn.disabled = true
+
+        // Async request to server to save question
+
+        input.value = ''
+        input.className = ''
+        submitBtn.disabled = false
+    }
+}
