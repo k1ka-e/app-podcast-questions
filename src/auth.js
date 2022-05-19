0a71de936287a429753379035fc792f94e4f929b
Kristina <kzatesova@gmail.com>
2022-05-19 16:23:05 +0500

    commit

--- src/auth.js
+++ src/auth.js
@@ -20,17 +20,17 @@ export function getAuthForm() {
 }
 
 export function authWithEmailAndPassword(email, password) {
-    const apiKey = 'AIzaSyADgzRG8ctfxCRJjZ_a2yizGv02rYjI3Yg'
-   return fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
-       method: 'POST',
-       body: JSON.stringify({
-           email, password,
-           returnSecureToken: true
-       }),
-       headers: {
-           'Content-Type': 'application/json'
-       }
-   })
-   .then(response => response.json())
-   .then(data => data.idToken)
+  const apiKey = 'AIzaSyADgzRG8ctfxCRJjZ_a2yizGv02rYjI3Yg'
+  return fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
+    method: 'POST',
+    body: JSON.stringify({
+      email, password,
+      returnSecureToken: true
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(data => data.idToken)
 }
--- src/question.js
+++ src/question.js
@@ -1,19 +1,19 @@
 export class Question {
   static create(question) {
-      fetch('https://podcast-app-b41af-default-rtdb.firebaseio.com/question.json', {
-          method: 'POST',
-          body: JSON.stringify(question),
-          headers: {
-              'Content-Type': 'application/json'
-          }
+      fetch('https://podcast-app-b41af-default-rtdb.firebaseio.com/questions.json', {
+        method: 'POST',
+        body: JSON.stringify(question),
+        headers: {
+          'Content-Type': 'application/json'
+        }
       })
-      .then(response => response.json())
-      .then(response => {
+        .then(response => response.json())
+        .then(response => {
           question.id = response.name
           return question
-      })
-      .then(addToLocalStorage)
-      .then(Question.renderList)
+        })
+        .then(addToLocalStorage)
+        .then(Question.renderList)
   }
 
   static fetch(token) {
@@ -21,55 +21,55 @@ export class Question {
       return Promise.resolve('<p class="error">У вас нет токена</p>')
     }
     return fetch(`https://podcast-app-b41af-default-rtdb.firebaseio.com/questions.json?auth=${token}`)
-      .then(response => response.json())
-      .then(response => {
-        if (response && response.error) {
-          return `<p class="error">${response.error}</p>`
-        }
+    .then(response => response.json())
+    .then(response => {
+      if (response && response.error) {
+        return `<p class="error">${response.error}</p>`
+      }
 
-        return response ? Object.keys(response).map(key => ({
-          ...response[key],
-          id: key
-        })) : []
-      })
-  }
+      return response ? Object.keys(response).map(key => ({
+        ...response[key],
+        id: key
+      })) : []
+    })
+}
 
-  static renderList() {
-    const questions = getQuestionsFromLocalStorage()
+static renderList() {
+  const questions = getQuestionsFromLocalStorage()
 
-    const html = questions.length
-      ? questions.map(toCard).join('')
-      : `<div class="mui--text-headline">Вы пока ничего не спрашивали</div>`
-  
-      const list = document.getElementById('list')
+  const html = questions.length
+    ? questions.map(toCard).join('')
+    : `<div class="mui--text-headline">Вы пока ничего не спрашивали</div>`
 
-      list.innerHTML = html
-  }
+  const list = document.getElementById('list')
 
-  static listToHTML(questions) {
-    return questions.length
+  list.innerHTML = html
+}
+
+static listToHTML(questions) {
+  return questions.length
     ? `<ol>${questions.map(q => `<li>${q.text}</li>`).join('')}</ol>`
     : '<p>Вопросов пока нет</p>'
-  }
+}
 }
 
 function addToLocalStorage(question) {
-  const all = getQuestionsFromLocalStorage()
-  all.push(question)
-  localStorage.setItem('question', JSON.stringify(all))
+const all = getQuestionsFromLocalStorage()
+all.push(question)
+localStorage.setItem('questions', JSON.stringify(all))
 }
 
 function getQuestionsFromLocalStorage() {
-  return JSON.parse(localStorage.getItem('question') || '[]')
+return JSON.parse(localStorage.getItem('questions') || '[]')
 }
 
 function toCard(question) {
-  return `
+return `
   <div class="mui--text-black-54">
     ${new Date(question.date).toLocaleDateString()}
     ${new Date(question.date).toLocaleTimeString()}
   </div>
   <div>${question.text}</div>
   <br>
-  `
+`
 }
